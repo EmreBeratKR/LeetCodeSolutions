513ac1d3d647c335edf15fc0558765af58ecb085
Emre Berat Kıran <doctoremre7@gmail.com>
2023-02-08 01:15:24 +0300

    118 - Pascal's Triangle

--- Solutions/0118 - Pascal's Triangle/Solution.cs
+++ Solutions/0118 - Pascal's Triangle/Solution.cs
@@ -1,7 +1,28 @@
-namespace LeetCodeSolutions.Solutions._0118___Pascal_s_Triangle
+using System.Collections.Generic;
+
+namespace LeetCodeSolutions.Solutions
 {
     public partial class Solution
     {
-        
+        public IList<IList<int>> Generate(int numRows) 
+        {
+            var result = new List<IList<int>> {new List<int>(){1}};
+
+            for (var i = 1; i < numRows; i++)
+            {
+                result.Add(new List<int>());
+
+                result[i].Add(1);
+
+                for (var j = 1; j < i; j++)
+                {
+                    result[i].Add(result[i - 1][j - 1] + result[i - 1][j]);
+                }
+
+                result[i].Add(1);
+            }
+
+            return result;
+        }
     }
 }
