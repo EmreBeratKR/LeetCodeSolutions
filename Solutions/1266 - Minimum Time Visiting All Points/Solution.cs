eadeb3b9e5d3a7409d4f4576362b82fbdec7567b
EmreBeratKR <emreberatkrn@gmail.com>
2023-12-03 22:52:58 +0300

    [add] 1266 - Minimum Time Visiting All Points

---
+++ Solutions/1266 - Minimum Time Visiting All Points/Solution.cs
@@ -0,0 +1,10 @@
+namespace EmreBeratKR.LeetCodeSolutions
+{
+    public partial class Solution 
+    {
+        public int MinTimeToVisitAllPoints(int[][] points)
+        {
+            return default;
+        }
+    }
+}
