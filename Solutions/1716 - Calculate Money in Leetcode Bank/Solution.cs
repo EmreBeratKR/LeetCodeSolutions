7d93b75557ee45b0a45c862461b3157bc9addb00
Emre Berat Kıran <doctoremre7@gmail.com>
2023-12-06 16:03:40 +0300

    [add] 1716 - Calculate Money in Leetcode Bank

---
+++ Solutions/1716 - Calculate Money in Leetcode Bank/Solution.cs
@@ -0,0 +1,10 @@
+namespace EmreBeratKR.LeetCodeSolutions
+{
+    public partial class Solution 
+    {
+        public int TotalMoney(int n) 
+        {
+            return default;    
+        }
+    }
+}
