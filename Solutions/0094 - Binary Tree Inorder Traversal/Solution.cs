ef2104a09797120115dc0232482c748f366e9740
EmreBeratKR <emreberatkrn@gmail.com>
2023-12-09 11:59:00 +0300

    [solve] 0094 - Binary Tree Inorder Traversal

--- Solutions/0094 - Binary Tree Inorder Traversal/Solution.cs
+++ Solutions/0094 - Binary Tree Inorder Traversal/Solution.cs
@@ -4,9 +4,67 @@ namespace EmreBeratKR.LeetCodeSolutions
 {
     public partial class Solution
     {
-        public IList<int> InorderTraversal(TreeNode root) 
+        public IList<int> InorderTraversal(TreeNode root)
         {
-            return default;    
+            return InorderTraversalWithIteration(root);
+        }
+
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+        // Auxiliary Space Complexity: O(n)
+        private IList<int> InorderTraversalWithRecursion(TreeNode root)
+        {
+            var values = new List<int>();
+            
+            InorderTraversalRecursionStep(root, values);
+
+            return values;
+        }
+
+        private void InorderTraversalRecursionStep(TreeNode root, IList<int> values)
+        {
+            if (root is null) return;
+            
+            InorderTraversalRecursionStep(root.left, values);
+            values.Add(root.val);
+            InorderTraversalRecursionStep(root.right, values);
+        }
+
+        private IList<int> InorderTraversalWithIteration(TreeNode root)
+        {
+            var values = new List<int>();
+            
+            if (root is null) return values;
+            
+            var nodeStack = new Stack<TreeNode>();
+            var ignoreLeft = false;
+
+            nodeStack.Push(root);
+
+            while (nodeStack.Count > 0)
+            {
+                var currentNode = nodeStack.Peek();
+
+                if (currentNode.left is null || ignoreLeft)
+                {
+                    values.Add(currentNode.val);
+                    nodeStack.Pop();
+
+                    ignoreLeft = currentNode.right is null;
+                    
+                    if (currentNode.right is not null)
+                    {
+                        nodeStack.Push(currentNode.right);
+                    }
+                    
+                    continue;
+                }
+
+                nodeStack.Push(currentNode.left);
+            }
+
+            return values;
         }
         
         
